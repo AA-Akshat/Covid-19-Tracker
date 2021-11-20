c76ea33f0c7e060ce1cf5603ead38ad10e65cea7
Akshat Aggarwal <77603321+AA-Akshat@users.noreply.github.com>
2021-11-21 03:17:20 +0530

    Add files via upload

---
+++ build/precache-manifest.af6963c3b0c58479608b521a077b4e08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7040fc7271cca8805f2c594995bedee6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "3e8b92d5fd4824d55463",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/js/2.ad8e3933.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.ad8e3933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e8b92d5fd4824d55463",
+    "url": "/static/js/main.ad95a084.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
